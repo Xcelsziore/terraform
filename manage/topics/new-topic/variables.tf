5c457afc08460ff015b10889a001662d77e5dc3f
xcelsziore <aiswarianair90@gatech.edu>
2020-05-01 17:52:51 -0700

    Minor updates

--- manage/topics/new-topic/variables.tf
+++ manage/topics/new-topic/variables.tf
@@ -1,4 +1,4 @@
-#adding new varibles.tf file
+#adding new varibles.tf file to store changes about dns_domain
 variable "dnsimple_domain" {
   description = "The domain we are creating a record for."
 }
