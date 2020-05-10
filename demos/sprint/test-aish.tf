4106a5fc47f0a6a858f5dce843597e5ea6fe8a5b
xcelsziore <aiswarianair90@gatech.edu>
2020-05-10 03:01:20 -0700

    Changes to test-aish.tf
    Changes to test-aish.tf

--- demos/sprint/test-aish.tf
+++ demos/sprint/test-aish.tf
@@ -4,6 +4,9 @@ variable "aws_secret_key" {}
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
+  foo = bar
+  abc = xyz
+  test_configuration_one = test_configuration_two
   region = "us-east-1"
 }
 
